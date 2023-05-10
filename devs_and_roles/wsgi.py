27ed5ff8c7a2b9b529f4c7412db528cb97e7e7bc
jeremyhsimons <>
2023-05-10 15:11:40 +0000

    Install django libraries and create app.

---
+++ devs_and_roles/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for devs_and_roles project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devs_and_roles.settings')
+
+application = get_wsgi_application()
