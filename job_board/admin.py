6ddaf544e61041c37f77399e553d7d13fd84c401
jeremyhsimons <>
2023-05-11 10:35:16 +0000

    Add jobposting view and create templates.

--- job_board/admin.py
+++ job_board/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from .models import JobPosting
 from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
 @admin.register(JobPosting)
 class JobPostingAdmin(SummernoteModelAdmin):
 
     list_filter = ('approved', 'reported', 'created_on')
     list_display = ('title', 'created_on', 'approved', 'reported')
     search_fields = ['title', 'company_overview', 'job_description',
-                    'requirements', 'benefits'
-                    ]
+                     'requirements', 'benefits']
+    actions = ['approve_job_posting', 'disapprove_job_posting']
+
+    def approve_job_posting(self, request, queryset):
+        queryset.update(approved=True)
+
+    def disapprove_job_posting(self, request, queryset):
+        queryset.update(approved=False)
--- job_board/views.py
+++ job_board/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import JobPosting
 
-# Create your views here.
+
+class JobPostingList(generic.ListView):
+    model = JobPosting
+    queryset = JobPosting.objects.filter(approved=True).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 12
