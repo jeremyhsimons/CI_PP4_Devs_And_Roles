2a3dd4094e4c4718e41ac94809506f8b19fe8020
jeremyhsimons <>
2023-05-12 11:05:19 +0000

    Install allauth and copy templates.

--- devs_and_roles/urls.py
+++ devs_and_roles/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('job_board.urls'), name='job_board_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
