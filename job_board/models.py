9ed9d514ee1f29a8399442f030c43ee63d248b49
jeremyhsimons <>
2023-05-15 11:08:44 +0000

    Create job application model.

--- job_board/models.py
+++ job_board/models.py
@@ -36,3 +36,32 @@ class JobPosting(models.Model):
 
     def get_deadline(self):
         return self.closing_date
+
+
+class JobApplication(models.Model):
+    """
+    A class to represent the data required in a
+    job application to one of the postings
+    on the site.
+    """
+    candidate = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="job_application"
+    )
+    job_posting = models.ForeignKey(
+        JobPosting, on_delete=models.CASCADE, related_name="job_application"
+    )
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone = models.IntegerField(max_length=20)
+    linkedin = models.CharField(max_length=200)
+    github_username = models.CharField(max_length=200)
+    why_company = models.TextField(max_length=2000)
+    why_role = models.TextField(max_length=2000)
+    why_you = models.TextField(max_length=2000)
+    supporting_docs = models.FileField(blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.pk
