4d321fff4a9b8ae869f6734e876b6a83514925ee
jeremyhsimons <>
2023-05-31 18:26:22 +0000

    Add new styles to auth/contact pages.

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views import generic, View
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from .models import ContactMessage
 from .forms import ContactForm
 
 
-class CreateContactMessage(generic.CreateView):
+class CreateContactMessage(generic.CreateView, SuccessMessageMixin):
     """
     A class for handling the contact page.
     """
@@ -24,7 +26,11 @@ class CreateContactMessage(generic.CreateView):
             if request.user:
                 form.instance.user = request.user
             form.save()
+            messages.success(
+                request, 'Your message has been sent to the site admin')
             return redirect('home')
         else:
             form = ContactForm()
+            messages.error(
+                request, 'We could not process your message. Please try again.')
             return redirect('contact')
