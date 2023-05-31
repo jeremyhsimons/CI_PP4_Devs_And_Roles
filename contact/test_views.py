575be813027ac94e8d74ab233135bf5c8059fbae
jeremyhsimons <>
2023-05-31 19:48:45 +0000

    Add setup and teardown functions for contact test.

---
+++ contact/test_views.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Internal
+from .models import ContactMessage
+from .forms import ContactForm
+from .views import CreateContactMessage
+
+
+class TestCreateContactMessage(TestCase):
+    """
+    A test to check that the CreateContactMessage view
+    works as expected.
+    """
+    def setUp(self):
+        # set up user for the test
+        self.user = User.objects.create(
+            username='testuser101',
+            password='hellotestuser101',
+            email='tester@email.com',
+            id='101'
+        )
+        self.user.save()
+        self.user.set_password('hellotestuser101')
+        self.user.save()
+    
+    def tearDown(self):
+        self.user.delete()
