95f0cf2b880441310122d60dee716cd9ca181507
jeremyhsimons <>
2023-05-31 19:00:34 +0000

    create tests for contact form

--- contact/models.py
+++ contact/models.py
@@ -7,13 +7,13 @@ class ContactMessage(models.Model):
     A class representing the data sent to the db
     when a user submits a contact form to the site.
     """
-    full_name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
+    full_name = models.CharField(max_length=200, blank=False)
+    email = models.EmailField(max_length=200, blank=False)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True,
         blank=True, related_name='contact_message')
     date_sent = models.DateTimeField(auto_now_add=True)
-    message = models.TextField()
+    message = models.TextField(blank=False)
     read = models.BooleanField(default=False)
 
     class Meta:
---
+++ contact/test_forms.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from .forms import ContactForm
+
+
+class TestContactForm(TestCase):
+    """
+    A test suite to check the contact form 
+    functions correctly.
+    """
+    def test_name_is_required(self):
+        form = ContactForm({'name': ''})
+        self.assertFalse(form.is_valid())
+    
+    def test_email_is_required(self):
+        form = ContactForm({'email': ''})
+        self.assertFalse(form.is_valid())
+    
+    def test_message_is_required(self):
+        form = ContactForm({'message': ''})
+        self.assertFalse(form.is_valid())
