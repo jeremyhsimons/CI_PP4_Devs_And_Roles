cba40ff6ae15bd27d2e9ecb59b0ad39cc3367068
jeremyhsimons <>
2023-06-13 15:26:29 +0000

    Implement github api for view profile page.

--- static/js/github.js
+++ static/js/github.js
@@ -2,18 +2,18 @@
 
 function userInformationHTML(user) {
     return `
-    <h2>${user.name}
+    <h2 class="black-text">${user.name}
         <span class="small-name">
             (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
         </span>
     </h2>
     <div class="gh-content">
-        <div class="gh-avatar">
+        <div class="gh-avatar light-green-bg">
             <a href="${user.html_url}" target="_blank">
                 <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}">
             </a>
         </div>
-        <p>
+        <p class="black-text">
             Followers: ${user.followers} - Following: ${user.following} <br> 
             Repos: ${user.public_repos}
         </p>
@@ -34,7 +34,7 @@ function repoInformationHTML(repos) {
 
     return `
     <div class="clearfix repo-list">
-        <p>
+        <p class="black-text">
             <strong>Repo List:</strong>
         </p>
         <ul>
@@ -50,13 +50,13 @@ function fetchGitHubInformation(event) {
 
     const username = $("#gh-username").val();
     if (!username) {
-        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        $("#gh-user-data").html(`<h2 class="black-text">Please enter a GitHub username</h2>`);
         return;
     }
 
     $("#gh-user-data").html(
-        `<div id="loader">
-            <img src="assets/css/loader.gif" alt="loading">
+        `<div class="progress">
+            <div class="indeterminate"></div>
         </div>`
     );
 
@@ -72,15 +72,22 @@ function fetchGitHubInformation(event) {
         }, function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
-                    `<h2>No info found for user ${username}</h2>`);
+                    `<h2 class="black-text">No info found for user ${username}</h2>`);
             } else if (errorResponse.status === 403) {
                 const resetTime = new Date(errorResponse.getResponseHeader("X-RateLimit-Reset") * 1000);
-                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+                $("#gh-user-data").html(`<h4 class="black-text">Too many requests made to GitHub API; please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
-                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+                    `<h2 class="black-text">Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         }
     );
 }
+
+$(document).ready(fetchGitHubInformation)
+
+// Event listener for input change
+$(document).ready(function () {
+    $("#gh-username").on("input", fetchGitHubInformation);
+});
