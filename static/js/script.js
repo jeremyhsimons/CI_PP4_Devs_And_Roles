d38d0216e98472ccd555e104f5c25b0fc4edfea2
jeremyhsimons <>
2023-06-28 11:06:07 +0000

    refactor custom JS to remove errors from console.

--- static/js/script.js
+++ static/js/script.js
@@ -1,72 +1,147 @@
-//Initialise modals
-document.addEventListener('DOMContentLoaded', function () {
+function initializeModals() {
     let modals = document.querySelectorAll('.modal');
     let modalsInstances = M.Modal.init(modals);
-});
-
-//Initialise dropdown
-document.addEventListener('DOMContentLoaded', function () {
-    let dropdowns = document.querySelectorAll('.dropdown-trigger');
-    let dropdownsInstances = M.Dropdown.init(dropdowns);
-});
-
-//Initialise sidenav
-document.addEventListener('DOMContentLoaded', function () {
-    let sidenavs = document.querySelectorAll('.sidenav');
-    var sidenavInstances = M.Sidenav.init(sidenavs);
-});
-
-//Fix bug that doesn't auto-apply materialize styles to textareas
-let textareas = document.querySelectorAll('.textarea');
-if(textareas.length > 0){
-    for (let i = 0; i < textareas.length; i++) {
-        textareas[i].className = 'materialize-textarea';
-    }
-};
-
-//Alter all form labels to be a darker shade.
-let labels = document.querySelectorAll('label');
-if (labels.length > 0) {
-    for (let i = 0; i < labels.length; i++) {
-        labels[i].className = 'black-text';
-    }
-};
-
-//Remove asterisks from forms
-let asterisks = document.querySelectorAll('.asterisk');
-for (let asterisk of asterisks) {
-    asterisk.remove();
 }
 
-//Messages disappear after 3 seconds.
-setTimeout(function () {
-    let message = document.getElementById("msg");
-    message.remove();
-}, 3000);
+function initializeDropdowns() {
+    let dropdowns = document.querySelectorAll('.dropdown-trigger');
+    let dropdownsInstances = M.Dropdown.init(dropdowns);
+}
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Alert the user to their inability to change applications once submitted.
+function initializeSidenavs() {
+    let sidenavs = document.querySelectorAll('.sidenav');
+    var sidenavInstances = M.Sidenav.init(sidenavs);
+}
+
+function fixTextareas() {
+    let textareas = document.querySelectorAll('.textarea');
+    if (textareas.length > 0) {
+        for (let i = 0; i < textareas.length; i++) {
+            textareas[i].className = 'materialize-textarea';
+        }
+    };
+}
+
+function fixLabels() {
+    let labels = document.querySelectorAll('label');
+    if (labels.length > 0) {
+        for (let i = 0; i < labels.length; i++) {
+            labels[i].className = 'black-text';
+        }
+    };
+}
+
+function fixAsterisks() {
+    let asterisks = document.querySelectorAll('.asterisk');
+    for (let asterisk of asterisks) {
+        asterisk.remove();
+    }
+}
+
+function messageTimeout() {
+    setTimeout(function () {
+        let message = document.getElementById("msg");
+        if (message != null) {
+            message.remove();
+        }
+    }, 3000);
+}
+
+function applicationToast() {
     const applyButton = document.getElementById('apply');
     applyButton.addEventListener('mouseenter', () => {
         console.log(applyButton);
         M.toast({ html: 'WARNING: Once you submit, You cannot change your application!', classes: 'red darken-4' });
     });
-});
+}
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Alert the user to the permanent deletion of account.
+function deletionToast() {
     const deleteButton = document.getElementById('delete');
     deleteButton.addEventListener('mouseenter', () => {
         console.log('hello');
         M.toast({ html: 'WARNING: This action is not reversible.', classes: 'red darken-4' });
     });
+}
+
+function fixCheckboxJobSeeker() {
+    let jobSeekerDiv = document.getElementById('div_id_job_seeker');
+    let jobSeekerInput = document.getElementById('id_job_seeker');
+    jobSeekerDiv.replaceWith(jobSeekerInput);
+}
+
+function fixCheckboxRecruiter() {
+    let recruiterDiv = document.getElementById('div_id_recruiter');
+    let recruiterInput = document.getElementById('id_recruiter');
+    recruiterDiv.replaceWith(recruiterInput);
+}
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    try {
+        initializeModals();
+    } catch(error){
+        //pass - no errors reported to console
+    }
+
+    try {
+        initializeDropdowns();
+    } catch (error) {
+        //pass - no errors reported to console
+    }
+
+    try {
+        initializeSidenavs();
+    } catch (error) {
+        //pass - no errors reported to console
+    }
+
+    try {
+        fixTextareas();
+    } catch (error) {
+        //pass - no errors reported to console
+    }
+
+    try {
+        fixLabels();
+    } catch (error) {
+        //pass - no errors reported to console
+    }
+
+    try {
+        fixAsterisks();
+    } catch (error) {
+        //pass - no errors reported to console
+    }
+
+    try {
+        messageTimeout();
+    } catch (error) {
+        //pass - no errors reported to console
+    }
+
+    try {
+        applicationToast();
+    } catch (error) {
+        //pass - no errors reported to console
+    }
+
+    try {
+        deletionToast();
+    } catch (error) {
+        //pass - no errors reported to console
+    }
+
+    try {
+        fixCheckboxJobSeeker();
+    } catch (error) {
+        //pass - no errors reported to console
+    }
+
+    try {
+        fixCheckboxRecruiter();
+    } catch (error) {
+        //pass - no errors reported to console
+    }
 
 });
-
-let jobSeekerDiv = document.getElementById('div_id_job_seeker');
-let jobSeekerInput = document.getElementById('id_job_seeker');
-jobSeekerDiv.replaceWith(jobSeekerInput);
-
-let recruiterDiv = document.getElementById('div_id_recruiter');
-let recruiterInput = document.getElementById('id_recruiter');
-recruiterDiv.replaceWith(recruiterInput);
